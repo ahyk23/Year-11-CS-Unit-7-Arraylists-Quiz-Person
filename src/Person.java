d7705ea150c8e38f77b9226adc1e215b6efe6fe4
alaynakwan <ahyk23@student.cis.edu.hk>
2023-03-07 12:48:34 +0800

    Classes are completed.

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,39 @@
 public class Person {
-
     private String firstName;
     private String lastName;
     private int age;
 
+    public Person(String f, String l, int a){
+        firstName = f;
+        lastName = l;
+        age = a;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setFirstName(String f){
+        firstName = f;
+    }
+
+    public void setLastName(String l){
+        lastName = l;
+    }
+
+    public void setAge(int a){
+        age = a;
+    }
+
+    public String toString(){
+        return firstName + lastName + ", age " + age;
+    }
 }
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,30 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
-
+        for (int i = 0; i < allPersons.size(); i++) {
+            if (person.getLastName().compareTo(allPersons.get(i).getLastName()) <= 0) {
+                allPersons.add(i, person);
+            }
+        }
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> minors = new ArrayList<>();
+        for (int i = 0; i < allPersons.size(); i++){
+            if (allPersons.get(i).getAge() < 18){
+                minors.add(allPersons.get(i));
+            }
+        }
+        return minors;
     }
 
     public boolean checkLastName(String lastName) {
-
+        boolean check = false;
+        for (int i = 0; i < allPersons.size(); i++){
+            if (lastName.equals(allPersons.get(i).getLastName())){
+                check = true;
+            }
+        }
+        return check;
     }
 }
